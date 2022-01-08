431b3e116a17cca63d77ba7cadf0d59d4d23707f
HMP1993 <mr.momen.mrm@gmail.com>
2022-01-08 23:21:46 +0330

    sidebar content rearranged

--- src/components/Dashboard/listItems.tsx
+++ src/components/Dashboard/listItems.tsx
@@ -1,64 +1,67 @@
 import * as React from "react";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import PeopleIcon from "@mui/icons-material/People";
-import LocalShippingRoundedIcon from "@mui/icons-material/LocalShippingRounded";
-import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
-import ReceiptLongRoundedIcon from "@mui/icons-material/ReceiptLongRounded";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import ListAltIcon from "@mui/icons-material/ListAlt";
-import IconButton from "@mui/material/IconButton";
+import { Divider } from "@mui/material";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import AddBusinessIcon from "@mui/icons-material/AddBusiness";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import CategoryIcon from "@mui/icons-material/Category";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import PlaylistAddCheckIcon from "@mui/icons-material/PlaylistAddCheck";
 
 export const mainListItems = (
   <div>
+    {/* Customers */}
     <ListItem>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="CUSTOMERS" />
-      <IconButton edge="end">
-        <AddCircleIcon />
-      </IconButton>
-      <IconButton edge="end">
-        <ListAltIcon />
-      </IconButton>
+      <ListItemText primary="CUSTOMERS" typeof="h1" />
     </ListItem>
+    <ListItem button>
+      <PersonAddIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Add Customers" />
+    </ListItem>
+    <ListItem button>
+      <PersonSearchIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Customers List" />
+    </ListItem>
+    <Divider />
+    {/* Suppliers */}
     <ListItem>
-      <ListItemIcon>
-        <LocalShippingRoundedIcon />
-      </ListItemIcon>
       <ListItemText primary="SUPPLIERS" />
-      <IconButton edge="end">
-        <AddCircleIcon />
-      </IconButton>
-      <IconButton edge="end">
-        <ListAltIcon />
-      </IconButton>
     </ListItem>
+    <ListItem button>
+      <AddBusinessIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Add Suppliers" />
+    </ListItem>
+    <ListItem button>
+      <StorefrontIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Suppliers List" />
+    </ListItem>
+    <Divider />
+    {/* Product */}
     <ListItem>
-      <ListItemIcon>
-        <CategoryRoundedIcon />
-      </ListItemIcon>
       <ListItemText primary="PRODUCTS" />
-      <IconButton edge="end">
-        <AddCircleIcon />
-      </IconButton>
-      <IconButton edge="end">
-        <ListAltIcon />
-      </IconButton>
     </ListItem>
+    <ListItem button>
+      <CategoryIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Add Products" />
+    </ListItem>
+    <ListItem button>
+      <CategoryIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Products List" />
+    </ListItem>
+    <Divider />
+    {/* Orders */}
     <ListItem>
-      <ListItemIcon>
-        <ReceiptLongRoundedIcon />
-      </ListItemIcon>
       <ListItemText primary="ORDERS" />
-      <IconButton edge="end">
-        <AddCircleIcon />
-      </IconButton>
-      <IconButton edge="end">
-        <ListAltIcon />
-      </IconButton>
+    </ListItem>
+    <ListItem button>
+      <PlaylistAddIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="Add orders" />
+    </ListItem>
+    <ListItem button>
+      <PlaylistAddCheckIcon style={{ minWidth: "55px" }} />
+      <ListItemText primary="orders List" />
     </ListItem>
   </div>
 );
